31b8b2c6f4fd2000ccec539e54f59bd94d283ea4
priyank rai <type2email@gmail.com>
2023-08-09 17:59:18 +0530

    cheching CICD on: push

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = require("express")();
 app.get("/", (req, res) => {
     res.status(200).send("Hello World 🚀")
 })
+
+app.get("/afterChange", (req, res) => {
+    res.status(200).send("Hello World AfterChange 🚀")
+})
 app.listen(process.env.PORT || 3000, () => {
     console.log('🖥 server running on http://localhost:' + (process.env.PORT || 3000))
 })
